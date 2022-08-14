056312010e3393a2fe29b2336ee364ae60595f0d
Toma <thomasmarques911@gmail.com>
2022-08-15 01:49:08 +0200

    test defined

--- test/index_test.js
+++ test/index_test.js
@@ -7,12 +7,8 @@ describe("createRequest", () => {
     context("successful calls", () => {
         const requests = [
             {
-                name: "Match does not exist",
-                testData: { id: jobID, data: { matchId: 0 } },
-            },
-            {
                 name: "Should be 0",
-                testData: { id: jobID, data: { matchId: 415990 } },
+                testData: { id: jobID, data: { matchId: 416100 } },
                 rep: 0,
             },
             {
@@ -38,39 +34,33 @@ describe("createRequest", () => {
         })
     })
 
-    // context("error calls", () => {
-    //     const requests = [
-    //            { name: "ID not supplied", testData: { data: { matchId: 1 } } },
-    //         { name: "empty body", testData: {} },
-    //         { name: "empty data", testData: { data: {} } },
-    //         {
-    //             name: "base not supplied",
-    //             testData: { id: jobID, data: { quote: "USD" } },
-    //         },
-    //         {
-    //             name: "quote not supplied",
-    //             testData: { id: jobID, data: { base: "ETH" } },
-    //         },
-    //         {
-    //             name: "unknown base",
-    //             testData: { id: jobID, data: { base: "not_real", quote: "USD" } },
-    //         },
-    //         {
-    //             name: "unknown quote",
-    //             testData: { id: jobID, data: { base: "ETH", quote: "not_real" } },
-    //         },
-    //     ]
+    context("error calls", () => {
+        const requests = [
+            {
+                name: "Match does not exist",
+                testData: { id: jobID, data: { matchId: 0 } },
+            },
+            { name: "empty data", testData: { id: jobID, data: {} } },
+            {
+                name: "Bad arg",
+                testData: { id: jobID, data: { matchId: "USD" } },
+            },
+            {
+                name: "Bad keyword",
+                testData: { id: jobID, data: { base: 1000 } },
+            },
+        ]
 
-    //     requests.forEach((req) => {
-    //         it(`${req.name}`, (done) => {
-    //             createRequest(req.testData, (statusCode, data) => {
-    //                 assert.equal(statusCode, 500)
-    //                 assert.equal(data.jobRunID, jobID)
-    //                 assert.equal(data.status, "errored")
-    //                 assert.isNotEmpty(data.error)
-    //                 done()
-    //             })
-    //         })
-    //     })
-    // })
+        requests.forEach((req) => {
+            it(`${req.name}`, (done) => {
+                createRequest(req.testData, (statusCode, data) => {
+                    assert.equal(statusCode, 500)
+                    assert.equal(data.jobRunID, jobID)
+                    assert.equal(data.status, "errored")
+                    assert.isNotEmpty(data.error)
+                    done()
+                })
+            })
+        })
+    })
 })
